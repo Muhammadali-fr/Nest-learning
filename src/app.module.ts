3eb54366fbff6762448d84de21c861a293fa5b41
Muhammadali-fr <boburjonr934@gmail.com>
2025-04-23 21:05:00 +0500

    posts api

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { MailerService } from './mailer/mailer.service';
 import { MailerModule } from './mailer/mailer.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, MailerModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, MailerModule, AuthModule, PostsModule],
   controllers: [AppController],
   providers: [AppService, MailerService],
 })
